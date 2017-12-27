ab5cda54ac867a03e1fac6d11db3954f9c271ad4
Cory Grunkemeyer <cory.grunkemeyer@robotsandpencils.com>
2017-12-27 10:28:56 -0700

    Silver voting works.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 const http = require('http')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const request = require('request')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -27,15 +28,19 @@ Airtable.configure({
 
 var base = Airtable.base(process.env.AIRTABLE_BASE)
 
+// QUERY AIRTABLE
+
 base('Imported table').select({
-	// Selecting the first 3 records in Grid view:
+	// Selecting the first 500 records in Grid view:
 	maxRecords: 500,
 	view: "Grid view"
 }).eachPage(function page(records, fetchNextPage) {
 	// This function (`page`) will get called for each page of records.
 
 	records.forEach(function(record) {
-			console.log('Retrieved', record.get('Email'))
+		if (record.get('Silver') > 0) {
+			missionsReq(record.get('Email'))
+		}
 	});
 
 	// To fetch the next page of records, call `fetchNextPage`.
@@ -46,6 +51,21 @@ base('Imported table').select({
 }, function done(err) {
 	if (err) { console.error(err); return; }
 })
+
+// MISSION WEBHOOK REQUEST
+var missionsReq = function (email) {
+	var missionsHook = 'https://api.missions.ai/webhook_trigger/e4f351c0-eb25-11e7-b75f-0a580a1c0534?auth_token=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MTQzOTMzNjksInN1YiI6ImU0ZjM1MWMwLWViMjUtMTFlNy1iNzVmLTBhNTgwYTFjMDUzNCJ9.sktZUDjN-25S89-PYPslg-l44CEmkya6TleIxP1SYUi2cIZK9PfTkLVHLg7laPiv3sejuZ6KdYZQO8IlirzK4A'
+
+	var missionsHookBody = '{ "person": "' + email + '"}'
+	missionsHookBody = (JSON.parse(missionsHookBody))
+
+	request.post({
+		url: missionsHook,
+		form: missionsHookBody
+	}, function(error, response, body){
+		console.log('statusCode:', response && response.statusCode)
+	});
+}
 
 
 // MIDDLEWARE
@@ -68,7 +88,6 @@ router.get('/', function(req, res) {
 });
 
 app.use('/', router)
-
 
 
 // START THE SERVER
